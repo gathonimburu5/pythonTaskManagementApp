ef0e469160b1149ab8d81008a312b2da3cc2e70f
Mburu Gathoni <paul.mburu@ngenx.io>
2025-04-17 16:51:06 +0300

    task query list

--- application/controllers/home_controller.py
+++ application/controllers/home_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from application.services.task_service import Tasks_Services
 
 home_blue = Blueprint("home_blue", __name__)
@@ -6,5 +6,6 @@ taskService = Tasks_Services()
 
 @home_blue.route("/", methods=["GET", "POST"])
 def home_index():
-    tasks = taskService.gettingAllTask()
-    return render_template("task.html", tasks = tasks)
+    if request.method == "GET":
+        tasks = taskService.gettingAllTask()
+        return render_template("task.html", tasks = tasks)
