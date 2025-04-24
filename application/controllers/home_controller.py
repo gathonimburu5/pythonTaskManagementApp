519e16d7965297ca705142ad7e0a0f0d202b2470
Mburu Gathoni <paul.mburu@ngenx.io>
2025-04-24 15:40:10 +0300

    task management comment service update

--- application/controllers/home_controller.py
+++ application/controllers/home_controller.py
@@ -9,3 +9,41 @@ def home_index():
     if request.method == "GET":
         tasks = taskService.gettingAllTask()
         return render_template("task.html", tasks = tasks)
+
+    if request.method == "POST":
+        data = request.form
+        taskService.createTaskRecords(data)
+        return redirect(url_for('home_blue.home_index'))
+
+@home_blue.route("/start/<int:id>", methods=["GET", "POST"])
+def start_page(id):
+    details = taskService.getTaskPerId(id)
+    if request.method == "GET":
+        return render_template("start_modal.html", details = details)
+
+    if request.method == "POST":
+        data = request.form
+        taskService.startTask(data, id)
+        return redirect(url_for('home_blue.home_index'))
+
+@home_blue.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_page(id):
+    taskDetails = taskService.getTaskPerId(id)
+    if request.method == "GET":
+        return render_template("edit_modal.html", taskDetails = taskDetails)
+
+    if request.method == "POST":
+        data = request.form
+        taskService.updateTask(data, id)
+        return redirect(url_for('home_blue.home_index'))
+
+@home_blue.route("/comment/<int:id>", methods=["GET", "POST"])
+def comment_page(id):
+    tasks = taskService.getTaskPerId(id)
+    if request.method == "GET":
+        return render_template("comment_modal.html", tasks = tasks)
+
+    if request.method == "POST":
+        data = request.form
+        taskService.taskComments(data, id)
+        return redirect(url_for('home_blue.home_index'))
--- application/services/task_service.py
+++ application/services/task_service.py
@@ -8,16 +8,53 @@ class Tasks_Services():
 
     def createTaskRecords(self, data):
         new_task = TaskTbl(
-            Title = data.get('title'),
-            Description = data.get('description'),
-            Status = data.get('status'),
-            Hours = data.get('hours'),
-            PlannedStartDate = data.get('planned_start_date'),
-            PlannedEndDate = data.get('planned_end_date'),
-            CreatedOn = datetime.utcnow(),
-            CreatedBy = 1
+            title = data.get('title'),
+            description = data.get('description'),
+            status = data.get('status'),
+            hours = data.get('hours'),
+            planned_start_date = data.get('planned_start_date'),
+            planned_end_date = data.get('planned_end_date'),
+            created_on = datetime.utcnow(),
+            created_by = 1
         )
         db.session.add(new_task)
         db.session.commit()
+
+    def getTaskPerId(self, id):
+        return TaskTbl.query.get_or_404(id)
+
+    def updateTask(self, data, id):
+        task_to_edit = TaskTbl.query.get_or_404(id)
+        task_to_edit.title = data.get('title')
+        task_to_edit.status = data.get('status')
+        task_to_edit.hours = data.get('hours')
+        task_to_edit.planned_start_date = data.get('planned_start_date')
+        task_to_edit.planned_end_date = data.get('planned_end_date')
+        task_to_edit.description = data.get('description')
+        task_to_edit.modified_on = datetime.utcnow()
+        task_to_edit.modified_by = 1
+        db.session.commit()
+
+    def startTask(self, data, id):
+        task_detail = TaskTbl.query.get_or_404(id)
+        task_detail.actual_start_date = data.get('actual_start_date')
+        task_detail.actual_end_date = data.get('actual_end_date')
+        db.session.commit()
+
+    def taskComments(self, data, id):
+        task_record = TaskTbl.query.get_or_404(id)
+        comment_data = CommentTbl(
+            task_id = task_record.id,
+            title = data.get('title'),
+            description = data.get('description'),
+            created_on = datetime.utcnow(),
+            created_by = 1
+        )
+
+        db.session.add(comment_data)
+        db.session.commit()
+
+    def getCommentList(self):
+        return CommentTbl.query.all()
 
 
