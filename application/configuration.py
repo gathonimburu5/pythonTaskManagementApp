154ed86d6e57a455f7e7424fcdc5d34c9405e7b8
Mburu Gathoni <paul.mburu@ngenx.io>
2026-02-05 14:51:32 +0300

    adding docker file and docker compose files in the task manage

--- application/configuration.py
+++ application/configuration.py
@@ -5,13 +5,13 @@ class Config():
     HOST = "0.0.0.0"
     DB_USERNAME = "postgres"
     DB_PASSWORD = quote("@Paulmburu5")
-    POSTGRES_URL = "127.0.0.1"
+    POSTGRES_URL = "task_db"
     POSTGRES_DB = "task_management_db"
     POSTGRES_PORT = 5432
     DB_URL = 'postgresql://{user}:{pswd}@{url}:{port}/{db}?application_name=TaskManagement'.format(user=DB_USERNAME, pswd=DB_PASSWORD, url=POSTGRES_URL, port=POSTGRES_PORT, db=POSTGRES_DB)
 
     SQLALCHEMY_DATABASE_URI = DB_URL
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_BINDS = { 'task_management_db': DB_URL }
 
     SECRET_KEY = secrets.token_hex(16)
